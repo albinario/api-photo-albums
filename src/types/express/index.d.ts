c2024fc23ebde63d8b8f5cb09f614d45fadeeb06
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-02-12 20:30:18 +0100

    add token parameter to Request object

---
+++ src/types/express/index.d.ts
@@ -0,0 +1,10 @@
+import { User } from '@prisma/client'
+import { JwtPayload } from '../../types'
+
+declare global {
+	namespace Express {
+		export interface Request {
+			token?: JwtPayload
+		}
+	}
+}
