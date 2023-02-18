a37485a42295198b828c606d588d2dd23515b8a9
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-02-18 02:52:46 +0100

    update format of messages

--- src/validations/photo_validation.ts
+++ src/validations/photo_validation.ts
@@ -3,27 +3,27 @@ import { body } from 'express-validator'
 export const storeValidation = [
 	body('title')
 		.exists().withMessage("Missing").bail()
-		.isString().withMessage("Has to be a string").bail()
-		.isLength({ min: 3 }).withMessage("Has to be at least 3 characters"),
+		.isString().withMessage("Must be a string").bail()
+		.isLength({ min: 3 }).withMessage("Must be at least 3 characters"),
 	body('url')
 		.exists().withMessage("Missing").bail()
-		.isURL().withMessage("Has to be a url"),
+		.isURL().withMessage("Must be a url"),
 	body('comment')
 		.optional()
-		.isString().withMessage("Has to be a string").bail()
-		.isLength({ min: 3 }).withMessage("Has to be at least 3 characters")
+		.isString().withMessage("Must be a string").bail()
+		.isLength({ min: 3 }).withMessage("Must be at least 3 characters")
 ]
 
 export const updateValidation = [
 	body('title')
 		.optional()
-		.isString().withMessage("Has to be a string").bail()
-		.isLength({ min: 3 }).withMessage("Has to be at least 3 characters"),
+		.isString().withMessage("Must be a string").bail()
+		.isLength({ min: 3 }).withMessage("Must be at least 3 characters"),
 	body('url')
 		.optional()
-		.isURL().withMessage("Has to be a url"),
+		.isURL().withMessage("Must be a url"),
 	body('comment')
 		.optional()
-		.isString().withMessage("Has to be a string").bail()
-		.isLength({ min: 3 }).withMessage("Has to be at least 3 characters")
+		.isString().withMessage("Must be a string").bail()
+		.isLength({ min: 3 }).withMessage("Must be at least 3 characters")
 ]
